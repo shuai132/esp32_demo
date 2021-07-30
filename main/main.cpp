6f6ac4bf8e287f34533fab24fd4a996583c35535
liushuai <770722922@qq.com>
2021-07-30 23:52:07 +0800

    feat: esp32c3 kit

--- main/main.cpp
+++ main/main.cpp
@@ -1,14 +1,29 @@
-#include "chrome_game.h"
-#include "game/game_engine_port_esp32_idf.h"
+#include <thread>
+#include <driver/gpio.h>
+
+#define LED_R GPIO_NUM_3
+#define LED_G GPIO_NUM_4
+#define LED_B GPIO_NUM_5
 
 extern "C"
 void app_main() {
-    oled.begin();
-    ScreenConfig c{};
-    c.SCREEN_WIDTH = OLED_WIDTH;
-    c.SCREEN_HEIGHT = OLED_HEIGHT;
-    c.PER_CHAR_WIDTH = 6;
-    c.fps = 120;
-    c.canvas = new Screen;
-    start_game(&c);
+    printf("hello\n");
+
+    gpio_config_t io_conf = {
+            .pin_bit_mask = 1ULL << LED_R | 1ULL << LED_G | 1ULL << LED_B,
+            .mode = GPIO_MODE_OUTPUT,
+    };
+    gpio_config(&io_conf);
+
+    std::thread dd([]{
+        int count = 0;
+        for(;;) {
+            printf("thread: %d\n", count++);
+            gpio_set_level(static_cast<gpio_num_t>(LED_R + count % 3), 1);
+            std::this_thread::sleep_for(std::chrono::milliseconds (100));
+            gpio_set_level(static_cast<gpio_num_t>(LED_R + count % 3), 0);
+            std::this_thread::sleep_for(std::chrono::milliseconds (100));
+        }
+    });
+    dd.join();
 }
